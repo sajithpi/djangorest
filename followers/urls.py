23fd6fd0647f1a9856fbceafaf2a7460fb6c2481
sajithpi <sajithpalamanna@gmail.com>
2023-10-08 12:21:28 -0700

    added follow details

--- followers/urls.py
+++ followers/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
-from .views import FollowUser
+from .views import FollowUser, GetFollowData
 
 urlpatterns = [
     path('user/followed_by',FollowUser.as_view(),name='follow'),
+    path('user/followers-details',GetFollowData.as_view(),name='followers-details'), 
 ]
--- followers/views.py
+++ followers/views.py
@@ -12,7 +12,9 @@ from django.utils.http  import urlsafe_base64_encode, urlsafe_base64_decode
 from django.contrib.sites.shortcuts import get_current_site
 from django.urls import reverse
 from rest_framework.parsers import MultiPartParser
-from accounts.models import User, UserProfile, CoverPhoto, Interest
+from .models import Follower
+from django.shortcuts import get_object_or_404
+from accounts.models import User, UserProfile
 from drf_yasg import openapi
 from drf_yasg.utils import swagger_auto_schema
 import json
@@ -28,3 +30,25 @@ class FollowUser(GenericAPIView):
         return Response({'message':'success', 
                          'description':'User follow action success'},
                          status=status.HTTP_200_OK)
+    
+class GetFollowData(GenericAPIView):
+    def post(self,request):
+        user_id = request.data.get('user_id')
+        
+        # Ensure the user exists or return a 404 response if not found
+        user = get_object_or_404(User, id=user_id)
+        
+        # Calculate total followers and total following for the user
+        total_followers = Follower.objects.filter(following=user).count()
+        total_following = Follower.objects.filter(followed_by=user).count()
+        
+        # Get the list of users following the current user
+        users_following_current_user = Follower.objects.filter(following=user).values_list('followed_by', flat=True)
+
+        return Response({
+            'message': 'Success',
+            'description': 'User follow action success',
+            'total_followers': total_followers,
+            'total_following': total_following,
+            'users_following_current_user': users_following_current_user,
+        }, status=status.HTTP_200_OK)
