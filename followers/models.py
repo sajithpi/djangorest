b603092f7c968f42f097b9af204c5e90d95849f8
sajithpi <sajithpalamanna@gmail.com>
2023-10-09 10:28:32 -0700

    added followers list

--- followers/models.py
+++ followers/models.py
@@ -1,11 +1,11 @@
 from django.db import models
 from django.contrib.auth.models import AbstractBaseUser, BaseUserManager
 from rest_framework_simplejwt.tokens import RefreshToken
-from accounts.models import User
+from accounts.models import User, UserProfile
 
 class Follower(models.Model):
-    followed_by = models.ForeignKey(User, on_delete=models.CASCADE, related_name='followed_by')
-    following = models.ForeignKey(User, on_delete=models.CASCADE, related_name='following')
+    followed_by = models.ForeignKey(UserProfile, on_delete=models.CASCADE, related_name='followed_by')
+    following = models.ForeignKey(UserProfile, on_delete=models.CASCADE, related_name='following')
 
     def __str__(self):
-        return f"{self.followed_by.username} is following {self.following.username}"
+        return f"{self.followed_by.user.username} is following {self.following.user.username}"
--- followers/views.py
+++ followers/views.py
@@ -37,18 +37,28 @@ class GetFollowData(GenericAPIView):
         
         # Ensure the user exists or return a 404 response if not found
         user = get_object_or_404(User, id=user_id)
-        
+        user_profile = UserProfile.objects.get(user = user)
         # Calculate total followers and total following for the user
-        total_followers = Follower.objects.filter(following=user).count()
-        total_following = Follower.objects.filter(followed_by=user).count()
+        total_followers = Follower.objects.filter(following=user_profile).count()
+        total_following = Follower.objects.filter(followed_by=user_profile).count()
         
         # Get the list of users following the current user
-        users_following_current_user = Follower.objects.filter(following=user).values_list('followed_by', flat=True)
+        users_followed_by_current_user = Follower.objects.filter(following=user_profile).values_list('followed_by', flat=True)
+
+        #fetch username, and user profile picture of each user in the users_following_current_user list
+        followers_data = UserProfile.objects.filter(user__id__in=users_followed_by_current_user).values('user__username','user__id','profile_picture')
+
+        #get the list of users where the current user following
+        following_users_of_current_user = Follower.objects.filter(following=user_profile).values_list('following',flat=True)
+
+        #fetch username, and user profile picture of each user in the users_following_current_user list
+        following_data = UserProfile.objects.filter(user__id__in=following_users_of_current_user).values('user__username','user__id','profile_picture')
 
         return Response({
             'message': 'Success',
             'description': 'User follow action success',
             'total_followers': total_followers,
             'total_following': total_following,
-            'users_following_current_user': users_following_current_user,
+            'users_followed_by_users': followers_data,
+            'following_data':following_data,
         }, status=status.HTTP_200_OK)
