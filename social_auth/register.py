8a3eacaa2dbe9e4626b9c1d63a79ef33a9946674
sajith <sajith@teamioss.in>
2023-12-06 11:38:40 +0530

    updated code for login

--- social_auth/register.py
+++ social_auth/register.py
@@ -85,6 +85,7 @@ def register_social_user(provider, user_id, email, name):
         if provider == filtered_user_by_email[0].auth_provider:
             # registered_user_token = authenticate(email=email, password=SOCIAL_SECRET)
             user = User.objects.get(email=email)
+            print(f"hereeeeeeeeeee")
             refresh_token = RefreshToken.for_user(user)
             access_token = str(refresh_token.access_token)
             profile_status = check_profile_complete_status(email=email)
@@ -134,7 +135,7 @@ def register_social_user_for_android(provider, user_id, name):
     if filtered_user_by_username.exists():
         if provider == filtered_user_by_username[0].auth_provider:
             # registered_user_token = authenticate_for_android(name=username, password=SOCIAL_SECRET)
-            user = User.objects.get(username=username)
+            
             refresh_token = RefreshToken.for_user(user)
             access_token = str(refresh_token.access_token)
             
