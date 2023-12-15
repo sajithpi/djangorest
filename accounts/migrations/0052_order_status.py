1b63fcbd1909e3f1a90014cdfb67dc78b4e50096
sajith <sajith@teamioss.in>
2023-12-15 15:47:37 +0530

    updated model

--- accounts/migrations/0052_order_status.py
+++ accounts/migrations/0052_order_status.py
@@ -6,7 +6,7 @@ from django.db import migrations, models
 class Migration(migrations.Migration):
 
     dependencies = [
-        ('accounts', '0051_Package_Data_Migrations'),
+        ('accounts', '0051_package_price_package_type_alter_package_validity'),
     ]
 
     operations = [
