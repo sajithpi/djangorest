bd00544ef654ae4f12cbdba23fbb06e2461766a2
sajith <sajith@teamioss.in>
2024-01-05 11:20:36 +0530

    added interests for mobile

--- accounts/views.py
+++ accounts/views.py
@@ -71,17 +71,31 @@ class RegisterView(GenericAPIView):
                     user_profile.profile_picture = profile_photo_data
                     user_profile.save()
 
-                # Add interests
-                if interests_data:
-                    print(f"interests_data:{interests_data}")
-                    for interest_name in interests_data:
-                        print(f"interest_name:{interest_name}")
-                        if device == 'web':
+
+                if device == 'web':
+                    # Add interests
+                    if interests_data:
+                        print(f"interests_data:{interests_data}")
+                        for interest_name in interests_data:
+                            print(f"interest_name:{interest_name}")
                             interest = Interest.objects.get(name=interest_name)
-                        else:
-                            interest = Interest.objects.get(id=interest_name)
-                        if interest:
-                            user.interests.add(interest)
+                            
+                            if interest:
+                                user.interests.add(interest)
+                else:
+                                # Update user interests
+                    if 'interests' in request.data:
+                        print(f"interest data json:{request.data.get('interests')}")
+                        interests_data = request.data.get('interests', '[]')
+                        # interests_data = request.data.get('interests', '[]')
+                        if interests_data:
+                            for interest_name in interests_data:
+                                print(f"interest_name:{interest_name}")
+                                interest_name = interest_name.strip()
+                                interest= Interest.objects.get(id=interest_name)
+                                if interest:
+                                    # user.user_ interests.add(interest)
+                                    user.interests.add(interest)
                             
                 threading.Thread(target=welcome_email, args=(serializer.data['email'], serializer.data['username'],'register')).start()
 
