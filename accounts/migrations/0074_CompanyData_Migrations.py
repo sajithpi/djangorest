37229f9e8283a3044dbc4fd21eba5366275c881e
sajith <sajith@teamioss.in>
2024-06-13 10:44:13 +0530

    added migrations

--- accounts/migrations/0074_CompanyData_Migrations.py
+++ accounts/migrations/0074_CompanyData_Migrations.py
@@ -1,10 +1,24 @@
 from django.db import migrations
+from django.conf import settings
+from django.core.files import File
 from accounts.models import CompanyData
+import os
+
 def seed_packages(apps, schema_editor):
 
    
     
-    CompanyData.objects.create(privacy_policy='privacy policy dummy content', terms_and_conditions='terms and conditions dummy content',)
+    CompanyData.objects.create(company_logo=get_dummy_logo(), 
+                            privacy_policy='privacy policy dummy content',
+                            terms_and_conditions='terms and conditions dummy content',)
+    
+def get_dummy_logo():
+    # Example function to provide a dummy image file if company_logo is null
+    dummy_logo_path = os.path.join(settings.MEDIA_ROOT, 'company', 'dummy_logo.png')
+    if os.path.exists(dummy_logo_path):
+        with open(dummy_logo_path, 'rb') as f:
+            return File(f)
+    return None  # Handle case where dummy logo doesn't exist
     
 class Migration(migrations.Migration):
 
