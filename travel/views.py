78fdc0b67cfd9a02b5c686528c2c0b7f9ac127ae
sajith <sajith@teamioss.in>
2024-06-18 17:02:52 +0530

    added travel histories

--- travel/views.py
+++ travel/views.py
@@ -577,7 +577,80 @@ class MyTravelRequests(GenericAPIView):
         except Exception as e:
             # Handle any other unexpected exceptions
             return Response({'error': str(e)}, status=status.HTTP_500_INTERNAL_SERVER_ERROR)
+    def post(self, request):
+            """
+            Retrieve the list of requested users for the specified trip.
 
+            This endpoint returns a list of users associated with the provided tripId.
+
+            :param request: The HTTP request object.
+            :type request: rest_framework.request.Request
+            :return: A JSON response containing the list of requested users or an error message.
+            :rtype: rest_framework.response.Response
+            
+            """
+            try:
+                # Assuming trip_id is sent as a query parameter or in the request body
+                trip_id = request.data['tripId']
+                print(f"TRIP ID:{trip_id}")
+                trip = MyTrip.objects.get(id = trip_id)
+                # Fetch the travel request based on the provided trip_id
+                trip_requests = TravelRequest.objects.filter(trip=trip)
+                
+                user_list = []
+                
+                # Iterate over each travel request and extract relevant information
+                for trip_request in trip_requests:
+                    user_dict = {}
+                    user_dict['id'] = trip_request.id
+                    user_dict['user_id'] = trip_request.requested_user.user.id
+                    user_dict['trip_id'] = trip_request.trip.id
+                    user_dict['username'] = trip_request.requested_user.user.username
+                    user_dict['profile_pic'] = str(trip_request.requested_user.profile_picture)
+                    user_dict['description'] = trip_request.description
+                    user_dict['status'] = trip_request.status
+                    
+                    user_list.append(user_dict)
+                
+                print(f"USER LIST:{user_list}")
+                # Return the list of users in the response
+                return Response(user_list, status=status.HTTP_200_OK)
+            
+            except KeyError:
+                # Handle the case where 'trip_id' is not provided in the request
+                return Response({'error': 'Missing trip_id in the request'}, status=status.HTTP_400_BAD_REQUEST)
+            
+            except TravelRequest.DoesNotExist:
+                # Handle the case where no TravelRequest is found for the provided trip_id
+                return Response({'error': 'TravelRequest not found for the given trip_id'}, status=status.HTTP_404_NOT_FOUND)
+            
+            except Exception as e:
+                # Handle any other unexpected exceptions
+                return Response({'error': str(e)}, status=status.HTTP_500_INTERNAL_SERVER_ERROR)
+
+        @swagger_auto_schema(
+            request_body=openapi.Schema(
+                type=openapi.TYPE_OBJECT,
+                properties={
+                    'trip_request_id': openapi.Schema(type=openapi.TYPE_INTEGER, description="ID of the travel request"),
+                    'trip_status': openapi.Schema(type=openapi.TYPE_STRING, 
+                                                description="New status for the travel request",
+                                                enum=["ACCEPTED", "REJECTED", "PENDING"],  # Possible values for trip_status
+                                                ),
+            
+                    'trip_id': openapi.Schema(type=openapi.TYPE_INTEGER, description="ID of the trip"),
+                },
+                required=['trip_request_id', 'trip_status', 'trip_id'],
+            ),
+            responses={
+                200: 'Travel request status updated successfully',
+                400: 'Bad Request - Missing or invalid parameters',
+                404: 'Travel request not found for the given ID',
+                500: 'Internal Server Error',
+            },
+            tags=["Travel"],
+            operation_description="Update the status of a requested trip.",
+        )
     @swagger_auto_schema(
         request_body=openapi.Schema(
             type=openapi.TYPE_OBJECT,
